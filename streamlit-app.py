ca40bdc998e7da3bf3b22caf0878a58ded849afa
SmilingPranjal <140375486+SmilingPranjal@users.noreply.github.com>
2023-11-13 22:40:34 +0530

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,8 +1,14 @@
 import streamlit
 import pandas
+import requests
+
 
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.header('Breakfast Menu')
 streamlit.dataframe(my_fruit_list)
 
+streamlit.header("Fruityvice Fruit Advice!")
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
